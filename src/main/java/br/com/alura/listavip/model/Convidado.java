b75ed69972256f94c1dd0bffef0d1120ae62c1fd
Lucas Adams <adams.lucass@gmail.com>
2017-10-31 15:01:19 -0200

    modulo 3

--- src/main/java/br/com/alura/listavip/model/Convidado.java
+++ src/main/java/br/com/alura/listavip/model/Convidado.java
@@ -7,7 +7,7 @@ import javax.persistence.Id;
 import lombok.Data;
 
 @Entity(name = "convidado")
-public @Data class Convidado {
+public class Convidado {
 
 	@Id
 	@GeneratedValue
@@ -15,4 +15,46 @@ public @Data class Convidado {
 	private String nome;
 	private String email;
 	private String telefone;
+
+	public Convidado() {
+	}
+
+	public Convidado(String nome, String email, String telefone) {
+		this.nome = nome;
+		this.email = email;
+		this.telefone = telefone;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
 }
